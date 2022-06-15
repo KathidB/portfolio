ffca2db7b178de019f3ccf32c3e658e104ba397a
KathidB <Kathidbb@gmail.com>
2022-06-15 14:19:13 +0200

    code clean up

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -1,5 +1,4 @@
 // wyłącza na mobile animacje od AOS
-//podyktowanie większym wynikiem w Lighthouse.
 AOS.init({ disable: "mobile" });
 
 //////////////// hamburger menu ////////////////////////////////////
@@ -19,18 +18,10 @@ const menuActivation = () => {
 
 hamburgerBtn.addEventListener("click", menuActivation);
 
-//funckja odpowiada za zamkniecie menu po akcji na click.
-document.querySelectorAll(".nav-menu-ham__item").forEach((el) => {
-  el.addEventListener("click", () => {
-    hamburgerBtn.classList.toggle("active");
-    hamMenu.classList.toggle("move-menu");
-  });
-});
-
 ///////////////////////////funkcja counter box/////////////////////////
-
 // dzieki tej wartości odliczanie nie zaczyna sie od razu na ikonkach
 //gdy scrollujemy w dól, a o 250px niżej,aby nie przegapić animacji.
+
 const options = {
   rootMargin: "-150px",
 };
@@ -39,6 +30,7 @@ const options = {
 //do zmiennej finalnumber przypisujemy docelowa liczbę z countera w html
 //value - domyslnie mamy wpisane zero w stringu - dzieki temu bedzie to liczba.
 //speed - liczba nie moze być wyższa niż liczba podana w data-number w html!
+
 const startCounter = (entry) => {
   if (entry[0].isIntersecting) {
     counterItems.forEach((counter) => {
@@ -46,21 +38,17 @@ const startCounter = (entry) => {
         const finalNumber = counter.getAttribute("data-number");
         const value = parseInt(counter.textContent);
         const speed = finalNumber / 50;
-
         if (value < finalNumber) {
           counter.textContent = `${Math.floor(value + speed)} `;
-          //wartość zmienia predkosc wykonywana się countera
           setTimeout(updateCounter, 10);
         } else {
           counter.textContent = finalNumber;
         }
       };
-
       updateCounter();
     });
   }
 };
-
 const observer = new IntersectionObserver(startCounter, options);
 observer.observe(counterBox);
 
@@ -74,9 +62,15 @@ window.onscroll = () => {
   }
 };
 
+//funckja odpowiada za zamkniecie menu po akcji na click.
+document.querySelectorAll(".nav-menu-ham__item").forEach((el) => {
+  el.addEventListener("click", () => {
+    hamburgerBtn.classList.toggle("active");
+    hamMenu.classList.toggle("move-menu");
+  });
+});
 
-
-// footer
+// footer - akrualny rok
 const getCurrentYear = () => {
   footerSpan.textContent = new Date().getFullYear();
 };

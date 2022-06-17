1927751d1da41b19c2f586ccab437c844d6f7e09
KathidB <Kathidbb@gmail.com>
2022-06-17 15:37:17 +0200

    added new FAQ section - work still in progress

---
+++ src/js/accordion.js
@@ -0,0 +1,34 @@
+const accordion = document.querySelector(".accordion");
+const accordionBtns = document.querySelectorAll(".accordion-btn");
+
+function openAccordionItems() {
+  console.log(this);
+  if (this.nextElementSibling.classList.contains("active-accordion")) {
+    this.nextElementSibling.classList.remove("active-accordion");
+  } else {
+    closeAccordionItem();
+    this.nextElementSibling.classList.toggle("active-accordion");
+  }
+}
+
+const closeAccordionItem = () => {
+  const allActiveItems = document.querySelectorAll(".accordion-info");
+  allActiveItems.forEach((item) => item.classList.remove("active-accordion"));
+};
+
+const clickOutsideAccordion = (e) => {
+  if (
+    e.target.classList.contains("accordion-btn") ||
+    e.target.classList.contains("accordion-info") ||
+    e.target.classList.contains("accordion-info-text")
+  )
+    return;
+
+  closeAccordionItem();
+};
+
+accordionBtns.forEach((btn) =>
+  btn.addEventListener("click", openAccordionItems)
+);
+
+window.addEventListener("click", clickOutsideAccordion);
